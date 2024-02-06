33bd9e8e80252921b16c5250cf077fbd6c9a732f
adri.lewagon@gmail.com <adri.lewagon@gmail.com>
2024-02-06 15:26:48 +0000

    finance file ready

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,12 +1,10 @@
 SELECT 
     date_date
     ,COUNT(orders_id) AS nb_transactions
-    ,SUM(revenue) AS revenue
-    ,AVG(revenue) AS average_basket
-    ,SUM(margin) AS margin
-    ,SUM(operational_margin) as operational_margin
-FROM {{ref("int_orders_margin")}} 
-FULL JOIN {{ref("int_orders_operational")}} 
-USING(orders_id)
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,ROUND(SUM(revenue)/COUNT(orders_id),2) AS average_basket
+    ,ROUND(SUM(margin),2) AS margin
+    ,ROUND(SUM(operational_margin),2) as operational_margin
+FROM {{ref("int_orders_operational")}} 
 GROUP BY date_date
 ORDER BY date_date DESC
