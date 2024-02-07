40040b29c8f3a3a25f2def4a78635c1e84167f28
adri.lewagon@gmail.com <adri.lewagon@gmail.com>
2024-02-07 15:41:37 +0000

    typo

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,4 +1,4 @@
-{% macro margin_percent (revnue, purchase_cost) %}
+{% macro margin_percent (revenue, purchase_cost) %}
     ROUND((SAFE_DIVIDE(revenue - purchase_cost), revenue) * 100,2)
 
 {% endmacro %}
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,6 +2,7 @@ SELECT
     *
     ,s.quantity * p.purchase_price AS purchase_cost
     ,s.revenue - (s.quantity * p.purchase_price) AS margin
+    ,{{margin_percent(revenue,purchase_cost)}} AS margin_percent 
 FROM
     {{ ref("stg_raw__sales") }} s 
 JOIN
