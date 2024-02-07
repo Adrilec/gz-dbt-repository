91fd1af4101f16b5adc97b30505d523f58aa98d2
adri.lewagon@gmail.com <adri.lewagon@gmail.com>
2024-02-07 14:39:38 +0000

    added finance campaigns per day

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,11 @@
+SELECT 
+    date_date
+    ,ROUND(SUM(operational_margin-ads_cost),0) as ads_margin
+    ,SUM(average_basket) as average_basket
+    ,SUM(operational_margin) as operational_margin
+    ,SUM(ads_cost) as ads_cost
+FROM {{ref("int_campaigns_day")}}
+FULL JOIN {{ref("finance_days")}}
+USING (date_date)
+GROUP BY date_date
+ORDER BY date_date DESC
